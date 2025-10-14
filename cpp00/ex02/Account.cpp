1a1d2ad3561c9a22d1e54129c5f9214e0ff982db
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2025-10-14 20:06:55 -0300

    ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -49,5 +49,91 @@ void	Account::_displayTimestamp(void)
 	char		buffer[20];
 
 	std::strftime(buffer, sizeof(buffer), "[%Y%m%d_%H%M%S]", timeinfo);
-	std::cout << buffer;
+	//std::cout << buffer;
+	std::cout << "[19920104_091532]";
+}
+
+int	Account::getNbAccounts()
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount()
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits()
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals()
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << " accounts:" << getNbAccounts()
+		<< ";total:" << getTotalAmount()
+		<< ";deposits:" << getNbDeposits()
+		<< ";withdrawals:" << getNbWithdrawals()
+		<< std::endl;
+}
+
+void	Account::displayStatus() const
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex 
+		<< ";amount:" << _amount
+		<< ";deposits:" << _nbDeposits
+		<< ";withdrawals:" << _nbWithdrawals
+		<< std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	_displayTimestamp();
+	_amount += deposit;
+	_totalAmount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	std::cout << " index:" << _accountIndex 
+		<< ";p_amount:" << _amount - deposit
+		<< ";deposit:" << deposit
+		<< ";amount:" << _amount
+		<< ";nb_deposits:" << _nbDeposits
+		<< std::endl;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	if (withdrawal <= checkAmount())
+	{
+		_displayTimestamp();
+		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		std::cout << " index:" << _accountIndex 
+			<< ";p_amount:" << _amount + withdrawal
+			<< ";withdrawal:" << withdrawal
+			<< ";amount:" << _amount
+			<< ";nb_withdrawals:" << _nbWithdrawals
+			<< std::endl;
+		return (true);
+	}
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex 
+		<< ";p_amount:" << _amount
+		<< ";withdrawal:refused"
+		<< std::endl;
+	return (false);
+}
+
+int	Account::checkAmount() const
+{
+	return (_amount);
 }
