ea62de04a9f9a7660f09d90d13f634244da96c7b
Caio De Souza Marinho <caide-so@c1r8p3.42sp.org.br>
2025-11-19 21:31:57 -0300

    cpp05 implementation

--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 19:12:24 by caide-so          #+#    #+#             */
-/*   Updated: 2025/11/14 19:26:08 by caide-so         ###   ########.fr       */
+/*   Updated: 2025/11/19 21:31:09 by caide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,40 @@
 #define BUREAUCRAT_HPP
 
 #include <string>
+#include <exception>
 
 class	Bureaucrat
 {
 	private:
 		const std::string	_name;
-		int			_classification;
+		int			_grade;
 
 	public:
 		Bureaucrat();
 		Bureaucrat(const Bureaucrat &other);
 		Bureaucrat &operator=(const Bureaucrat &other);
 		~Bureaucrat();
+
+		Bureaucrat(std::string name, int grade);
 		
 		const std::string	getName() const;
-		int			getClassification() const;
+		int			getGrade() const;
+		void			incrementGrade();
+		void			decrementGrade();
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
 };
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &value);
 
 #endif
